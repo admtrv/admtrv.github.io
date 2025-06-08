64f37dbafaf835bbf11b36015a695859cb544c5f
Anton Dmitriev <tosha.dmitriefff@yandex.ru>
2025-06-08 10:44:45 +0200

    refactor: minor cleanup of site structure

--- resources/js/card.js
+++ resources/js/card.js
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", () => {
           const img = this.getAttribute("image");
           const alt = this.getAttribute("alt");
 
-          const template = document.getElementById("card-template").content.cloneNode(true);
+          const template = document.getElementById("project-card-template").content.cloneNode(true);
           const a = template.querySelector("a");
 
           a.href = href;
@@ -27,9 +27,57 @@ document.addEventListener("DOMContentLoaded", () => {
 
           template.querySelector("img").src = img;
           template.querySelector("img").alt = alt;
-          template.querySelector("h2").textContent = title;
-          template.querySelector("p").innerHTML = desc;
+          template.querySelector(".title").textContent = title;
+          template.querySelector(".description").innerHTML = desc;
 
+          this.appendChild(template);
+        }
+      }
+
+      class ArticleCard extends HTMLElement {
+        connectedCallback() {
+          const href = this.getAttribute("href");
+          const title = this.getAttribute("title");
+          const date = this.getAttribute("date");
+          const datetime = this.getAttribute("datetime") || date;
+          const desc = this.getAttribute("desc");
+          const img = this.getAttribute("image");
+          const alt = this.getAttribute("alt");
+      
+          const template = document.getElementById("article-card-template").content.cloneNode(true);
+          const a = template.querySelector("a");
+      
+          a.href = href;
+          a.setAttribute("onmouseover", `animationSetTitle('${title}', '${title}');`);
+          a.setAttribute("onmouseout", "animationClearTitle();");
+      
+          template.querySelector("img").src = img;
+          template.querySelector("img").alt = alt;
+          template.querySelector(".title").textContent = title;
+          template.querySelector(".date").textContent = date;
+          template.querySelector(".date").setAttribute("datetime", datetime);
+          template.querySelector(".description").innerHTML = desc;
+      
+          this.appendChild(template);
+        }
+      }   
+      
+      class BookCard extends HTMLElement {
+        connectedCallback() {
+          const title = this.getAttribute("title");
+          const author = this.getAttribute("author");
+          const review = this.getAttribute("review");
+          const img = this.getAttribute("image");
+          const alt = this.getAttribute("alt");
+      
+          const template = document.getElementById("book-card-template").content.cloneNode(true);
+      
+          template.querySelector("img").src = img;
+          template.querySelector("img").alt = alt;
+          template.querySelector(".title").textContent = title;
+          template.querySelector(".author").textContent = author;
+          template.querySelector(".review").innerHTML = review;
+      
           this.appendChild(template);
         }
       }
@@ -37,5 +85,12 @@ document.addEventListener("DOMContentLoaded", () => {
       if (!customElements.get("project-card")) {
         customElements.define("project-card", ProjectCard);
       }
+      if (!customElements.get("article-card")) {
+        customElements.define("article-card", ArticleCard);
+      }
+      if (!customElements.get("book-card")) {
+        customElements.define("book-card", BookCard);
+      }
+
     });
 });
